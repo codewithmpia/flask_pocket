3361693f43bc35177fcdefad10081f9a3d8a6bc5
PULUDISU Mpia Mimpiya <mpia-mimpiya.puludisu02@etud.univ-paris8.fr>
2025-01-11 21:08:51 +0100

    Finalisation de l'extension

--- flask_pocket.py
+++ flask_pocket.py
@@ -1,10 +1,78 @@
 from flask import Flask, g
 from pocketbase import PocketBase
+from pocketbase.errors import ClientResponseError
 
 
 class FlaskPocket:
+    """
+    FlaskPocket is an extension for Flask that integrates PocketBase, a backend service.
+    
+    Attributes:
+        client (PocketBase): The PocketBase client instance.
+        ClientResponseError (Exception): The exception class for handling client response errors.
+    
+    Example:
+    from flask import Flask
+    from flask_pocket import FlaskPocket
+
+    app = Flask(__name__)
+
+    app.config["POCKETBASE_URL"] = "your_pocketbase_url"
+    app.config["POCKETBASE_ADMIN_EMAIL"] = "your_admin_email"
+    app.config["POCKETBASE_ADMIN_PASSWORD"] = "your_admin_password"
+    pocket = FlaskPocket(app)
+
+    Queries:
+
+    1. GET ALL OBJECTS
+        try:
+            posts = pocket.collection("posts").get_full_list()
+        except pocket.ClientResponseError as e:
+            # Handle error
+            print(f"Error: {e}")
+
+    2. GET ONE OBJECT
+        try:
+            post = pocket.collection("posts").get_one(post_id)
+            # Generate image url
+            image_url = pocket.client.get_file_url(post, post.image)
+        except pocket.ClientResponseError as e:
+            # Handle error
+            print(f"Error: {e}")
+
+    3. CREATE 
+        try:
+            pocket.collection("contacts").create({
+                "name": name,
+                "email": email,
+                "message": message
+            })
+            return redirect(url_for("contact"))
+        except pocket.ClientResponseError as e:
+            # Handle error
+            print(f"Error: {e}")
+
+    4. EDIT
+        try:
+            pocket.collection("contacts").update(contact_id, {
+                "name": new_name,
+                "email": new_email,
+                "message": new_message
+            })
+        except pocket.ClientResponseError as e:
+            # Handle error
+            print(f"Error: {e}")
+
+    5. DELETE
+        try:
+            pocket.collection("contacts").delete(contact_id)
+        except pocket.ClientResponseError as e:
+            # Handle error
+            print(f"Error: {e}")
+    """
     def __init__(self, app=None):
         self.client = None
+        self.ClientResponseError = ClientResponseError  # Rendre l'exception accessible
         if app is not None:
             self.init_app(app)
 
